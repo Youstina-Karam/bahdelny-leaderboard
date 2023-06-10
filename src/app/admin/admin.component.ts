545609d725ad594c7d768d99be3dd4d06bef101e
Mina Karam <minakaram.me@gmail.com>
2023-06-10 23:37:05 +0300

    Update Admin & leaderboard components

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -11,4 +11,21 @@ export class AdminComponent  implements OnInit {
 
   ngOnInit() {}
 
+  newPlayer = {
+    name: '',
+    score: 0,
+  };
+
+  players = [
+    { name: 'Player 1', score: 100 },
+    { name: 'Player 2', score: 200 },
+    { name: 'Player 3', score: 150 },
+    // Existing players
+  ];
+
+  addPlayer() {
+    this.players.push({ name: this.newPlayer.name, score: this.newPlayer.score });
+    this.newPlayer.name = '';
+    this.newPlayer.score = 0;
+  }
 }
--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -10,5 +10,11 @@ export class LeaderboardComponent  implements OnInit {
   constructor() { }
 
   ngOnInit() {}
-
+  
+  players = [
+    { name: 'Player 1', score: 100 },
+    { name: 'Player 2', score: 200 },
+    { name: 'Player 3', score: 150 },
+    // Add more players as needed
+  ];
 }
